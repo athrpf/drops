e5188b711925cdbcedcd0fca39388655ef69f690
Marcus Soemers <soemers@igpm.rwth-aachen.de>
2003-05-15 11:18:51 +0000

    * Weitere Uebergabeparameter eingebaut:   - Anzahl der Teilintervalle pro Koordinatenrichtung (mrx, mry, mrz).   - Zeitintegration mit one-step-theta (theta).   - Anzahl der max. Iterationen und Genauigkeit fuer CG (cgiter, cgtol).

--- poisson/matlab/test.m
+++ poisson/matlab/test.m
@@ -11,15 +11,30 @@ nu= 10.0;
 dt= 0.001;
 
 % Laenge des Quaders in x-, y- und z-Richtung
-xl= 1; 
-yl= 1;
-zl= 1;
+xl= 1.0; 
+yl= 1.0;
+zl= 1.0;
 
-% Anzahl der Verfeinerungen
-mr= 2;
+% Anzahl der Teilintervalle pro Koordinatenrichtung 
+mrx= 4;
+mry= 4;
+mrz= 4;
 
 % Anzahl der Zeitschritte
 ndt= 100;
+
+% Zeitdiskretisierung ueber one-step-theta-scheme
+% theta= 1.0 -> impliziter Euler
+% theta= 0.5 -> Crank-Nicholson 
+% theta= 0.0 -> expliziter Euler
+theta= 0.5;
+
+% Toleranz fuer das CG-Verfahren
+cgtol= 10^(-4);
+% maximale Anzahl der CG-Iterationen
+cgiter= 500;
+
+
 
 % Parameter, der die Flaeche spezifiziert an der die 
 % Messungen durchgefuehrt werden
@@ -30,58 +45,19 @@ M= 0;
 
 % ----------------------------- Eingabedaten -------------------------------
 
-% Anzahl der Punkte pro Koordinatenrichtung
-npc= 2^mr+1;
-
-% Anzahl der Punkte pro Seitenflaeche 
-nfp= (npc)^2;
-
 % Anfangstemperatur
 % lexikographische Nummerierung der Gitterpunkte
-% von x=0 bis x=1
-T0= zeros(nfp, npc);
-%T0= [1 10 19
-%     2 11 20
-%     3 12 21
-%     4 13 22
-%     5 14 23
-%     6 15 24
-%     7 16 25
-%     8 17 26
-%     9 18 27];
+% von x=0 bis x=xl
+T0= zeros((mry+1)*(mrz+1), (mrx+1));
 
 % Daten auf den beiden gegenueberliegenden
-% Wuerfelseiten (x=0 & x=1)
+% Wuerfelseiten (x=0 & x=xl)
 % lexikographische Nummerierung der Gitterpunkte
-% fuer x=0 bzw. x=1
-S1= -4*ones(nfp, ndt+1);
-S2= 4*ones(nfp, ndt+1);
-
-%S1= [1 10 19 28 37
-%     2 11 20 29 38
-%     3 12 21 30 39
-%     4 13 22 31 40
-%     5 14 23 32 41
-%     6 15 24 33 42
-%     7 16 25 34 43
-%     8 17 26 35 44
-%     9 18 27 36 45]; 
-     
-%S1= [1 10 19 28
-%    2 11 20 29
-%    3 12 21 30
-%    4 13 22 31
-%    5 14 23 32
-%    6 15 24 33
-%    7 16 25 34
-%    8 17 26 35
-%    9 18 27 36]; 
-
-%T0= zeros(9, 81);
-%S1= 4*ones(81, 11);
-%S2= -4*ones(81, 11);
+% fuer x=0 bzw. x=xl
+S1= -4*ones((mry+1)*(mrz+1), ndt+1);
+S2= 4*ones((mry+1)*(mrz+1), ndt+1);
 
 
 % Loesung mit DROPS
-sol= ipdrops(nu, dt, xl, yl, zl, T0, S1, S2, M);
+sol= ipdrops(T0, S1, S2, M, xl, yl, zl, nu, mrx, mry, mrz, dt, theta, cgtol, cgiter);
 
