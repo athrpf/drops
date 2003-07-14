34717ae7039cc6955050c8e0f2ce30ae0037befc
Marcus Soemers <soemers@igpm.rwth-aachen.de>
2003-07-14 13:00:00 +0000

    ** Erweiterung der MATLAB-Schnittstelle um die beiden Parameter    Flag (Loesung mit oder ohne Quellterm) und BndRef (Anzahl der    Verfeinerungen auf zwei ausgezeichneten Raendern)

--- poisson/matlab/test.m
+++ poisson/matlab/test.m
@@ -11,14 +11,14 @@ nu= 10.0;
 dt= 0.001;
 
 % Laenge des Quaders in x-, y- und z-Richtung
-xl= 1.0; 
-yl= 1.0;
-zl= 1.0;
+xl= 1; 
+yl= 1;
+zl= 1;
 
 % Anzahl der Teilintervalle pro Koordinatenrichtung 
-mrx= 4;
-mry= 4;
-mrz= 4;
+mrx= 2;
+mry= 2;
+mrz= 2;
 
 % Anzahl der Zeitschritte
 ndt= 100;
@@ -30,34 +30,47 @@ ndt= 100;
 theta= 0.5;
 
 % Toleranz fuer das CG-Verfahren
-cgtol= 10^(-4);
+cgtol= 10^(-5);
 % maximale Anzahl der CG-Iterationen
 cgiter= 500;
-
-
 
 % Parameter, der die Flaeche spezifiziert an der die 
 % Messungen durchgefuehrt werden
 % (Schnitt durch den Wuerfel an der Stelle x=M)
 M= 0;
 
+% Parameter Flag steuert, ob das direkte Problem mit (Flag=1)
+% oder ohne (Flag=0) Quellterm geloest wird
+Flag= 0;
+
+% Parameter BndRef gibt die Anzahl der Randverfeinerungen fuer
+% die Raender x=0 & x=xl an
+BndRef= 4;
 
 
 % ----------------------------- Eingabedaten -------------------------------
 
+% Anzahl der Intervalle pro Koordinatenrichtung
+% nach der Verfeinerung (die Anfangstemperatur wird
+% fuer den feinsten Level uebergeben)
+RefIntX= mrx*2^BndRef;
+RefIntY= mry*2^BndRef;
+RefIntZ= mrz*2^BndRef;
+BndPtsYZ= (RefIntY+1)*(RefIntZ+1);
+
 % Anfangstemperatur
 % lexikographische Nummerierung der Gitterpunkte
 % von x=0 bis x=xl
-T0= zeros((mry+1)*(mrz+1), (mrx+1));
+T0= zeros(BndPtsYZ, RefIntX+1);
 
 % Daten auf den beiden gegenueberliegenden
 % Wuerfelseiten (x=0 & x=xl)
 % lexikographische Nummerierung der Gitterpunkte
 % fuer x=0 bzw. x=xl
-S1= -4*ones((mry+1)*(mrz+1), ndt+1);
-S2= 4*ones((mry+1)*(mrz+1), ndt+1);
+S1= -4*ones(BndPtsYZ, ndt+1);
+S2= 4*ones(BndPtsYZ, ndt+1);
 
 
 % Loesung mit DROPS
-sol= ipdrops(T0, S1, S2, M, xl, yl, zl, nu, mrx, mry, mrz, dt, theta, cgtol, cgiter);
+sol= ipdrops(T0, S1, S2, M, xl, yl, zl, nu, mrx, mry, mrz, dt, theta, cgtol, cgiter, Flag, BndRef);
 
