f4c4b949b4569d74985e3d4e23afdbb46633feaf
Marcus Soemers <soemers@igpm.rwth-aachen.de>
2003-12-29 14:20:04 +0000

    * Fehler in der Behandlung der Randterme (bei implizitem Euler) behoben

--- poisson/matlab/optimization/opt1D/solve_direct.m
+++ poisson/matlab/optimization/opt1D/solve_direct.m
@@ -3,7 +3,7 @@ function u = solve_direct(ic,bcx0,bcx1,tf,L)
 % Solve the heat equation using backward Euler
 
 % ntimes - number of temporal steps from t=0 to t=tf
-ntimes=size(bcx0,2);
+ntimes=size(bcx0,2)-1;
 % dt - time increment
 dt=tf/ntimes;
 % N - number of meshpoints in space
@@ -25,14 +25,13 @@ A = spdiags([e -2*e e], -1:1, N, N);
 A(1,2) = 2;
 A(N,N-1) = 2;
 
-dbc = zeros(N,ntimes);
+dbc = zeros(N,ntimes+1);
 dbc(1,:) = bcx0; 
 dbc(N,:) = bcx1;
 
 for k = 1 : ntimes
-	u(:,k+1) = (eye(N)-dt/dx^2*A)\(u(:,k)-2*dt/dx*dbc(:,k));
+	u(:,k+1) = (eye(N)-dt/dx^2*A)\(u(:,k)+2*dt/dx*dbc(:,k+1));
 end
-
 % plot temperature near x=0 as a function of time
 %figure(4)
 %plot(t,u(1,:),'b')
