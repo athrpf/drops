fbe0fddd25b276c6cac547b27f167353adcca719
Marcus Soemers <soemers@igpm.rwth-aachen.de>
2003-12-29 14:18:19 +0000

    * weitere Test-Funktionen hinzugefuegt

--- poisson/matlab/optimization/opt1D/q.m
+++ poisson/matlab/optimization/opt1D/q.m
@@ -2,15 +2,15 @@ function exflux = q(t)
 
 % different benchmark heat fluxes
 
-%if (0<t & t<=0.6)
+% if (0<t & t<=0.6)
 %    exflux=t;
-%elseif (0.6<t & t<=1.2)
+% elseif (0.6<t & t<=1.2)
 %    exflux=1.2-t;
-%else
+% else
 %    exflux=0;
-%end
+% end
 
-%if (t<0.3)
+% if (t<0.3)
 %     exflux=0;
 % elseif (t<0.4)
 %     exflux=10*(t-0.3);
@@ -27,6 +27,20 @@ function exflux = q(t)
 %    exflux= 1;
 %end
 
-exflux= sin(2*pi*t/1.4);
+%exflux= sin(2*pi*t/1.4);
+
+%exflux= sin(2*pi*t);
+
+% if (0<t & t<2)
+%     exflux= 2*exp(-1/(1-2*(abs(t-1))^2));
+% else
+%     exflux= 0;
+% end
+
+if (0<t & t<2)
+    exflux= 2*exp(-1/(1-abs(t-1)^2));
+else
+    exflux= 0;
+end
 
 return
