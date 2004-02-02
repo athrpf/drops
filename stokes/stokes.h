4295685ebff7cd42a1a92e3af972c2e3c667a671
Sven Gross <gross@igpm.rwth-aachen.de>
2004-02-02 16:22:33 +0000

    * enum BndCondT fuer Randbedingungen von stokes/stokes.h nach geom/builder.h   verschoben

--- stokes/stokes.h
+++ stokes/stokes.h
@@ -18,13 +18,6 @@
 namespace DROPS
 {
 
-enum BndCondT
-{
-    DirBC= 0, Dir0BC= 2,         // (in)hom. Dirichlet boundary conditions
-    NatBC= 1, Nat0BC= 3,         // (in)hom. natural   boundary conditions
-    OutflowBC= 3, WallBC= 2      // for convenience
-};
-
 class VelBndSegDataCL
 {
   public:
