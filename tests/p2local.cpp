994138d676df474fd5b164a3c361e36713e5bda7
Joerg Grande <grande@igpm.rwth-aachen.de>
2005-01-27 10:10:01 +0000

    **Pasto korrigiert.

--- tests/p2local.cpp
+++ tests/p2local.cpp
@@ -190,7 +190,7 @@ template<class T>
 
 
 template<class T>
-  LocalP2CL<T>::LocalP2CL<T>(const TetraCL& s, T (*f)(const Point3DCL&, double) , double t)
+  LocalP2CL<T>::LocalP2CL(const TetraCL& s, T (*f)(const Point3DCL&, double) , double t)
   : base_type( value_type(), NumDoFP2C)
 {
     this->Assign( s, f, t);
