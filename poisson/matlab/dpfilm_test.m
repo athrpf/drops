ef5093e9e78e082ca0f2c38249a3b4c3edef40e2
Sven Gross <gross@igpm.rwth-aachen.de>
2005-12-01 16:26:25 +0000

    * Plot der Loesung hinzugefuegt

--- poisson/matlab/dpfilm_test.m
+++ poisson/matlab/dpfilm_test.m
@@ -11,4 +11,32 @@ F   = zeros(Nx*Ny*Nz, Nt);
 Tsol= zeros(Nx*Ny*Nz, Nt-1);
 
 [MaxIter, Tsol]= ipfilm(T0, T_in, F, 0.26, lx, ly, lz, Nx-1, Ny-1, Nz-1, dt, Nt-1, 0.5, 1e-10, 5000, 77);
-  
+
+% plot the solution
+
+xd=linspace(0,lx,Nx);
+yd=linspace(0,ly,Ny);
+[Xd,Yd]=meshgrid(xd,yd);
+
+TempLast= reshape(Tsol(:,Nt-1),[Nz,Ny,Nx]);  % last time step
+Txy= reshape( TempLast(Nz/2,:,:), [Ny, Nx]); 
+
+pp=0;
+if pp==1  
+    %colormap(gray)  
+    surfl(Xd,Yd,Txy);
+    shading interp
+else
+    mesh(Xd,Yd,Txy);
+end
+
+xlabel x
+ylabel y    
+zlabel T
+
+figure(2)
+
+contour(Xd,Yd,Txy,10);
+% chi=1:10:100;
+% clabel(c(:,chi),h(chi));
+colorbar;
