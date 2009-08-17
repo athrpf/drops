e37b51e951673f61ef798e42012159941cb06437
Patrick Esser <pesser@igpm.rwth-aachen.de>
2009-08-17 14:15:24 +0000

    ** Zeitdiskretisierungsauswahl: Fehler behoben

--- levelset/twophasedrops.cpp
+++ levelset/twophasedrops.cpp
@@ -147,7 +147,7 @@ TimeDisc2PhaseCL<StokesProblemT>* CreateTimeDisc(StokesProblemT& Stokes, Levelse
         break;
         case 7 :
             return (new TrapezoidTimeDisc2PhaseCL<StokesProblemT, LevelSetSolverT>
-                        (Stokes, lset, *stokessolver, *lsetsolver, lsetmod, C.cpl_Tol, C.ns_Nonlinear, C.cpl_Projection, C.cpl_Stab, true));
+                        (Stokes, lset, *stokessolver, *lsetsolver, lsetmod, C.cpl_Tol, C.stk_Theta, C.lvs_Theta, C.ns_Nonlinear, C.cpl_Projection, C.cpl_Stab, true));
         break;
         case 8 :
             return (new EulerBackwardScheme2PhaseCL<StokesProblemT, LevelSetSolverT>
