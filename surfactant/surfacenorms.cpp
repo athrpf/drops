01df7393ca0728b19b2a6142f3e0ed54214f0aa0
Joerg Grande <grande@igpm.rwth-aachen.de>
2009-12-11 09:48:05 +0000

    **LGPL/Copyright im surfactant-Verzeichnis

--- surfactant/surfacenorms.cpp
+++ surfactant/surfacenorms.cpp
@@ -1,8 +1,27 @@
-//****************************************************************************
-// File:    brickflow.cpp                                                    *
-// Content: test case: one-phase flow in square pipe with oscillating inflow *
-// Author:  Sven Gross, Joerg Peters, Volker Reichelt, IGPM RWTH Aachen      *
-//****************************************************************************
+/// \file
+/// \brief Evaluation-tool for solutions produced by surfactant/surfactant.cpp
+/// \author LNM RWTH Aachen: Joerg Grande
+
+/*
+ * This file is part of DROPS.
+ *
+ * DROPS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * DROPS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with DROPS. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * 
+ * Copyright 2009 LNM/SC RWTH Aachen, Germany
+*/
+
 #include "surfactant/ifacetransp.h"
 #include "surfactant/params.h"
 #include "geom/builder.h"
