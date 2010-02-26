5d3d5777133aea99356ae223157703f735c8d03b
Patrick Esser <pesser@igpm.rwth-aachen.de>
2010-02-26 09:39:55 +0000

    ** Bei 3D-Levelset wird eine Exception geworfen, die Warnung hilft in den meisten Faellen nicht

--- num/interfacePatch.cpp
+++ num/interfacePatch.cpp
@@ -116,7 +116,7 @@ bool InterfacePatchCL::ComputeVerticesOfCut( Uint ch, bool compute_PQRS)
     // Warn in case of 3D-interface.
     if (num_sign_[1] == 4)
     {
-        std::cerr << "WARNING: InterfacePatchCL: found 3-dim. zero level set, grid is too coarse!" << std::endl;
+        throw DROPSErrCL("WARNING: InterfacePatchCL: found 3-dim. zero level set, grid is too coarse!");
         return false;
     }
     // first come the zero-vertices of the child ch_...
