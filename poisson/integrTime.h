a36a09897b9b496f3d1f68ce5afec467aa68522b
Liang Zhang <liang@igpm.rwth-aachen.de>
2012-01-30 15:02:04 +0100

    * time integration of ALE and SUPG, stick to the same mass matrix for different time step, codes need to be optimized

--- poisson/integrTime.h
+++ poisson/integrTime.h
@@ -60,11 +60,13 @@ class InstatPoissonThetaSchemeCL
     VecDescCL *_cplM, *_old_cplM;       // couplings with mass matrix M
     VecDescCL *_cplU;                   // couplings with convection matrix U
     VectorCL  _rhs;
+    VecDescCL *_new_cplA, *_new_b; 
     MLMatrixCL  _Lmat;                  // M + theta*dt*nu*A  = linear part
 
     double _theta, _dt;
     bool   _Convection;
     bool   _supg;
+    bool   _ale;
 
 
   public:
@@ -74,12 +76,15 @@ class InstatPoissonThetaSchemeCL
       _cplA( new VecDescCL), _old_cplA( new VecDescCL),
       _cplM( new VecDescCL), _old_cplM( new VecDescCL),
       _cplU( new VecDescCL),
-      _rhs( Poisson.b.RowIdx->NumUnknowns()), _theta( _param.get<double>("Time.Theta")), 
-      _Convection(_param.get<int>("PoissonCoeff.Convection")), _supg(_param.get<int>("Stabilization.SUPG"))
+      _rhs( Poisson.b.RowIdx->NumUnknowns()), 
+      _new_cplA( new VecDescCL), _new_b( new VecDescCL), _theta( _param.get<double>("Time.Theta")),
+      _Convection(_param.get<int>("PoissonCoeff.Convection")), _supg(_param.get<int>("Stabilization.SUPG")),
+      _ale(_param.get<int>("ALE.wavy"))
     {
       _old_b->SetIdx( _b->RowIdx);
       _cplA->SetIdx( _b->RowIdx); _old_cplA->SetIdx( _b->RowIdx);
       _cplM->SetIdx( _b->RowIdx); _old_cplM->SetIdx( _b->RowIdx);
+      _new_cplA->SetIdx( _b->RowIdx); _new_b->SetIdx( _b->RowIdx);
       _Poisson.SetupInstatRhs( *_old_cplA, *_old_cplM, _Poisson.x.t, *_old_b, _Poisson.x.t);
       if (_Convection)
       {
@@ -124,19 +129,27 @@ template <class PoissonT, class SolverT>
 void InstatPoissonThetaSchemeCL<PoissonT,SolverT>::DoStep( VecDescCL& v)
 {
   _Poisson.x.t+= _dt;
-  
-  if(_supg)
-  _Poisson.SetupInstatSystem( _Poisson.A, _Poisson.M, _Poisson.x.t);
-  
+  _rhs = _Poisson.A.Data * v.Data;
+  _rhs*= -_dt*(1.0-_theta); 
+
+  _rhs+=  _dt*(1.0-_theta)*(_old_b->Data)
+         +_dt*(1.0-_theta)* _old_cplA->Data;
+  //Update rhs
+  if(_supg||_ale) //update stiffness and massmatrix if necessary
+    _Poisson.SetupInstatSystem( _Poisson.A, _Poisson.M, _Poisson.x.t);
   _Poisson.SetupInstatRhs( *_cplA, *_cplM, _Poisson.x.t, *_b, _Poisson.x.t);
 
-  _rhs = _Poisson.A.Data * v.Data;
-  _rhs*= -_dt*(1.0-_theta);
-  _rhs+= _Poisson.M.Data*v.Data
-         + _dt*( _theta*_b->Data + (1.0-_theta)*(_old_b->Data))
-         + (_dt*(1.0-_theta)) * _old_cplA->Data + (_dt*_theta) * _cplA->Data
-         - _old_cplM->Data + _cplM->Data;
+  _Poisson.SetupInstatRhs( *_new_cplA, *_old_cplM, _Poisson.x.t-_dt, *_new_b, _Poisson.x.t-_dt);
 
+  _rhs +=_Poisson.M.Data*v.Data
+          -_old_cplM->Data
+          +_dt*_theta*_b->Data
+          + _dt*_theta*_cplA->Data
+          + _cplM->Data;
+  
+
+
+  
   if (_Convection)
   {
       _rhs+= (_dt*(1.0-_theta)) * (_cplU->Data - _Poisson.U.Data * v.Data );
